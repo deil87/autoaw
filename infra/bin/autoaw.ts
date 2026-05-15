f799aca912d058fc89c968669e6ece47eb6318df
Andrey Spiridonov <spirtik87@gmail.com>
2026-05-15 19:30:11 +0200

    Add GitHub Actions deploy workflow (eu-central-1)

--- infra/bin/autoaw.ts
+++ infra/bin/autoaw.ts
@@ -8,7 +8,7 @@ const app = new cdk.App();
 
 const env: cdk.Environment = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
+  region: process.env.CDK_DEFAULT_REGION ?? 'eu-central-1',
 };
 
 const storage = new StorageStack(app, 'AutoAwStorage', { env });
